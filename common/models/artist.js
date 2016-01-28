2d29fbba2c731e6b26876a69f4ee738556e730ae
David Olivari <david.olivari@gmail.com>
2016-01-28 06:45:01 +0100

    adding artist creation

--- common/models/artist.js
+++ common/models/artist.js
@@ -1,3 +1,41 @@
 module.exports = function(Artist) {
+  Artist.testadd = function(name, cb) {
+    Artist.findOne({
+      where: {
+        name: name
+      }
+    }, function(err, artist) {
+      if (artist == null) {
+        Artist.create({
+          name: name
+        }, function() {
+          cb(null, {
+            status: 'created'
+          });
+        });
+      } else {
+        cb(null, {
+          status: 'exist'
+        });
+      }
 
+    });
+  };
+  Artist.remoteMethod('testadd', {
+    http: {
+      path: '/testadd',
+      verb: 'get'
+    },
+    accepts: {
+      arg: 'name',
+      type: 'string',
+      http: {
+        source: 'query'
+      }
+    },
+    returns: {
+      arg: 'artist',
+      type: 'string'
+    }
+  })
 };
